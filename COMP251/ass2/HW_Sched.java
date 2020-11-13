37193138ec86cfc20e9cf6ea8b593b089a1d400e
Saikou Ceesay <ceesaysaikou72@gmail.com>
2020-11-13 18:19:58 -0500

    homework schedule implemented, passing all test cases [WIP]

--- COMP251/ass2/HW_Sched.java
+++ COMP251/ass2/HW_Sched.java
@@ -21,17 +21,13 @@ class Assignment implements Comparator<Assignment> {
 	@Override
 	public int compare(Assignment a1, Assignment a2) {
 		// should I order based on deadline or weights?????? //weight maximization
-		if (a1.deadline == a2.deadline) {
-			if (a1.weight > a2.weight)
-				return -1;
-			else if (a1.weight < a2.weight)
-				return 1;
-			else
-				return 0;
-		} else if (a1.deadline > a2.deadline)
-			return 1;
-		else
+		if (a1.weight > a2.weight) {
 			return -1;
+		} else if (a2.weight > a1.weight) {
+			return 1;
+		} else {
+			return a1.deadline < a2.deadline ? 1 : a1.deadline > a2.deadline ? -1 : 0;
+		}
 	}
 }
 
@@ -65,34 +61,58 @@ public class HW_Sched {
 		// If schedule[i] has a value -1, it indicates that the
 		// i'th timeslot in the schedule is empty
 		int[] schedule = new int[lastDeadline];
-		int j = 0; // tracks the index of assignments in the list
 		for (int i = 0; i < schedule.length; i++) {
 			schedule[i] = -1; // initialize all slots with -1
 		}
-		ArrayList<Assignment> l = get(Assignments);
-		schedule[0] = l.get(0).number; // greedy choice = the howework with highest weight
+
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+		for(int i = 0; i < Assignments.size(); ++i) {
+			int deadline = Assignments.get(i).deadline;
+			ArrayList<Integer> list = new ArrayList<>();
+			if(!map.containsKey(deadline)) {
+				list.add(deadline);
+				list.add(1);
+				map.put(deadline, list);
+			}else {
+				int count = 1 + map.get(deadline).get(1);
+				map.get(deadline).set(1, count);
+				map.put(deadline, map.get(deadline));
+			}
+		}
+
+		int j = 0;
 		for (int i = 0; i < schedule.length; ++i) {
-			// check if assignments don't overlap
-			if (i < l.size()) {
-				schedule[i] = l.get(i).number;
+			if(j < Assignments.size()) {
+				int deadline = Assignments.get(j).deadline;
+				int number  = Assignments.get(j).number;
+				int val = map.get(deadline).get(1);			// frequency
+				if(map.get(deadline).get(0) != 0) {		// deadline has to be > frequency
+					schedule[i] = number;
+					int count = map.get(deadline).get(0) - 1;
+					map.get(deadline).set(0, count);
+					j++;
+				}else{
+					for(int k = j; k < Assignments.size(); k++){
+						int deadline2 = Assignments.get(k).deadline;
+						int number2  = Assignments.get(k).number;
+						if(deadline != deadline2){
+							schedule[i] = number2;
+							j = k+1;
+							break;
+						}
+					}
+				}
+				
 			}
 		}
 		return schedule;
 	}
-
-	public ArrayList<Assignment> get(List<Assignment> l) {
-		HashSet<Assignment> seen = new HashSet<>();
-		ArrayList<Assignment> ll = new ArrayList<>();
-		ll.add(l.get(0));
-		seen.add(l.get(0));
-		for (int i = 1; i < l.size(); i++) {
-			if (l.get(i - 1).deadline != l.get(i).deadline) {
-				if (!seen.contains(l.get(i))) {
-					ll.add(l.get(i));
-					seen.add(l.get(i));
-				}
-			}
-		}
-		return ll;
-	}
 }
+
+
+
+
+
+
+
+
