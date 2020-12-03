85dfc89bd3748b537b02e5ffde6eefd9812ace48
Saikou Ceesay <ceesaysaikou72@gmail.com>
2020-12-02 21:02:11 -0500

    bellmand ford implemented but having index out of bound error

--- COMP251/ass3/BellmanFord.java
+++ COMP251/ass3/BellmanFord.java
@@ -34,7 +34,42 @@ public class BellmanFord {
          * If the node is not reachable from the source, the distance value must be
          * Integer.MAX_VALUE
          */
+        int V = g.getNbNodes();
+        distances = new int[V];
+        predecessors = new int[V];
+        this.source = source;
+        // step 1: intialize graph
+        for (int i = 0; i < V; ++i) {
+            distances[i] = Integer.MAX_VALUE;
+            predecessors[i] = -1001; // let -1001 represent null
+        }
 
+        // distance of source
+        distances[source] = 0;
+        ArrayList<Edge> edges = g.getEdges();
+        int E = edges.size();
+
+        // step 2: relax edges n - 1 times
+        for (int i = 0; i < V - 1; ++i) {
+            for (int j = 0; j < E; ++j) {
+                int weight = edges.get(j).weight; // weight
+                int u = edges.get(j).nodes[0]; // from
+                int v = edges.get(j).nodes[1]; // to
+                if (distances[u] + weight < distances[v]) {
+                    distances[v] = distances[u] + weight;
+                    predecessors[v] = u;
+                }
+            }
+        }
+        // step 3: check for negative-weight cycles
+        for (int j = 0; j < E; ++j) {
+            int weight = edges.get(j).weight; // weight
+            int u = edges.get(j).nodes[0]; // from
+            int v = edges.get(j).nodes[1]; // to
+            if (distances[u] + weight < distances[v]) {
+                throw new NegativeWeightException("The graph contains a negative cycle.");
+            }
+        }
     }
 
     public int[] shortestPath(int destination) throws PathDoesNotExistException {
@@ -42,7 +77,25 @@ public class BellmanFord {
          * Returns the list of nodes along the shortest path from the object source to
          * the input destination If not path exists an Error is thrown
          */
-        return null;
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(destination);
+        int temp = destination;
+        while (predecessors[temp] != -1001) {
+            temp = predecessors[temp];
+            list.add(temp);
+        }
+
+        if (list.size() == 1) {
+            throw new PathDoesNotExistException("There is no path between " + this.source + " and " + destination);
+        }
+        int[] arr = new int[list.size()];
+        int idx = 0;
+        for (int i = list.size() - 1; i >= 0; --i) {
+            arr[idx] = list.get(i);
+            idx++;
+        }
+        return arr;
     }
 
     public void printPath(int destination) {
